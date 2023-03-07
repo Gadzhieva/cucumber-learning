70227de47fbf1df3b714f75331b7083da9c1d097
Gadzhieva <minnigareevalr@gmail.com>
2023-03-07 23:40:13 +0000

    Initial commit

---
+++ src/test/java/com/gadzhieva/cucumberlearning/CalculatorTest.java
@@ -0,0 +1,9 @@
+package com.gadzhieva.cucumberlearning;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class CalculatorTest {
+
+}
---
+++ src/test/java/com/gadzhieva/cucumberlearning/model/Calculator.java
@@ -0,0 +1,7 @@
+package com.gadzhieva.cucumberlearning.model;
+
+public class Calculator {
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+}
